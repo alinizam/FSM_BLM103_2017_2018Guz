c89efc1c57db7e1629819974c99f6134b97d670c
alinizam <alinizam99@gmail.com>
2017-09-27 22:22:53 +0300

    Giriş Dersi

---
+++ BLM103_2017_2018_Guz/src/Ders01/Ornek2DegiskenlerGiris.java
@@ -0,0 +1,50 @@
+package Ders01;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author anizam
+ */
+public class Ornek2DegiskenlerGiris {
+
+    public static void main(String[] args) {
+        int yazilacakSayi;
+        yazilacakSayi = 1000;
+        System.out.println(yazilacakSayi);
+        yazilacakSayi = yazilacakSayi + 1;
+        System.out.println(yazilacakSayi);
+        yazilacakSayi = yazilacakSayi + 1;
+        System.out.println(yazilacakSayi);
+        yazilacakSayi = yazilacakSayi + 1;
+        System.out.println(yazilacakSayi);
+        yazilacakSayi = yazilacakSayi + 1;
+        System.out.println(yazilacakSayi);
+        yazilacakSayi = yazilacakSayi + 1;
+        System.out.println(yazilacakSayi);
+        yazilacakSayi = yazilacakSayi + 1;
+        System.out.println(yazilacakSayi);
+        yazilacakSayi = yazilacakSayi + 1;
+        System.out.println(yazilacakSayi);
+        yazilacakSayi = yazilacakSayi + 1;
+        System.out.println(yazilacakSayi);
+        yazilacakSayi = yazilacakSayi + 1;
+        System.out.println(yazilacakSayi);
+        yazilacakSayi = yazilacakSayi + 1;
+        System.out.println(yazilacakSayi);
+        yazilacakSayi = yazilacakSayi + 1;
+        System.out.println(yazilacakSayi);
+        yazilacakSayi = yazilacakSayi + 1;
+        System.out.println(yazilacakSayi);
+        yazilacakSayi = yazilacakSayi + 1;
+        System.out.println(yazilacakSayi);
+        yazilacakSayi = yazilacakSayi + 1;
+        System.out.println(yazilacakSayi);
+        
+
+    }
+
+}
---
+++ BLM103_2017_2018_Guz/src/Ders01/Ornek3DegiskenTanimSekil.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ders01;
+
+/**
+ *
+ * @author anizam
+ */
+public class Ornek3DegiskenTanimSekil {
+    public static void main(String[] args) {
+        int i=0, x=3;
+        int k;
+        i=3;
+        k=i;
+        System.out.println(i);
+    }
+}
